b9027b6791c774415671e41fd6479183ecf02df9
dev_eccentric <lefugdot@gmail.com>
2021-07-17 22:08:51 +0300

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+   
+   <div class="main-util w-screen h-screen flex overflow-hidden">
+
+      Worked!
+
+   </div>
+
+</template>
+
+<script lang="ts">
+
+   import { Component, Prop, Vue } from "nuxt-property-decorator";
+   
+   @Component({
+      name: 'index',
+      components: {
+      }
+   })
+   export default class index extends Vue {
+
+   } 
+
+</script>
---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import index from '~/pages/index.vue'
+
+Vue.use(Router);
+
+
+export function createRouter() {
+
+   return new Router({
+      mode: 'history',
+      routes: [
+         {
+            path: '/',
+            name: 'home',
+            component: index,
+         },
+      ],
+   
+   })
+}
